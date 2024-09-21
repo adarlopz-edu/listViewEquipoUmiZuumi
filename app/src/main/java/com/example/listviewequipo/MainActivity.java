8462375e56e9f03eea4afe1d92b3df884edbc50e
adarlopz-edu <a22100190@ceti.mx>
2024-09-20 20:30:38 -0500

    holi

--- app/src/main/java/com/example/listviewequipo/MainActivity.java
+++ app/src/main/java/com/example/listviewequipo/MainActivity.java
@@ -25,6 +25,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         //hola, esto lo escribio brad
+        //hi
         findViewById(R.id.play);
     }
 
